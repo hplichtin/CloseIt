34c4e614a418d7231f1d80998c53c32bf6c28ad1
Hans-Peter Lichtin <hans-peter.lichtin@outlook.com>
2021-09-12 08:59:15 +0200

    Initial Commit

---
+++ CloseIt/CloseItApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloseItApp.swift
+//  CloseIt
+//
+//  Created by Hans-Peter Lichtin on 12.09.21.
+//
+
+import SwiftUI
+
+@main
+struct CloseItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
